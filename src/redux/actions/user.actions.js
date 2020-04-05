fdd4c42da86df65066e9d959400e3677be9a3298
Micah Manning <micah.manning@newwave.io>
2020-04-05 14:25:11 -0400

    set user action type to constant in user.action

--- src/redux/actions/user.actions.js
+++ src/redux/actions/user.actions.js
@@ -1,6 +1,6 @@
 import { UserActionTypes } from '../types/user.types';
 
 export const setCurrentUser = (user) => ({
-	type    : 'SET_CURRENT_USER',
+	type    : UserActionTypes.SET_CURREENT_USER,
 	payload : user,
 });
