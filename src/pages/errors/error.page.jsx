b50d0447600095d989ccdcc0ae99b6a93f9843f2
Micah Manning <micah.manning@newwave.io>
2020-04-14 16:31:38 -0400

    minor updates to error page

--- src/pages/errors/error.page.jsx
+++ src/pages/errors/error.page.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+
+import './error.styles.scss';
+
 const ErrorPage = ({ type, timeStamp }) => {
 	const errorCode = `${type}@${timeStamp}`;
+	const mailTo = `mailto:manningmicah@gmail.com?subject= CrownTG-Error: ${errorCode}`;
 	return (
-		<div className='errorPage'>
-			<h1>{`Error: ${type}.`}</h1>
-			<div>
-				<p>Please, try back later, we seem to be experiencing issues.</p>
-				<p>Error Code: {`${errorCode}`}</p>
+		<div className='error-page'>
+			<h1 className='error-title'>{`Error: ${type}.`}</h1>
+			<div className='error-message'>
+				<p className='error-detail'>Please, try back later, we seem to be experiencing issues.</p>
+				<p className='error-detail'>Error Code: {`${errorCode}`}</p>
 			</div>
-			<a href={`mailto:manningmicah@gmail.com?subject= CrownTG-Error: ${errorCode}`}>Email Us about it!</a>
+			<a className='error-link' href={mailTo}>
+				Email us about it!
+			</a>
 		</div>
 	);
 };
